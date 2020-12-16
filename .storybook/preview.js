9a41a9429c2ae9593afd361c27de5274a76828a1
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2020-12-16 23:44:42 +0900

    設定

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: '#000000',
+      },
+    ],
+  },
 }
