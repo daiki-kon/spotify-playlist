a723e28d15f2f7d7871a1e8bc41884e5ada15ba0
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2020-12-23 01:15:12 +0900

    playlist card作成

---
+++ src/components/PlaylistCard.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from 'react';
+import { Image } from 'semantic-ui-react';
+import styled from 'styled-components';
+import 'semantic-ui-css/semantic.min.css';
+
+export type PlaylistCardProps = {
+  coverImageUrl: string;
+  playlistName: string;
+  description: string;
+  width: number;
+};
+
+const StyledCard = styled.div<{ width: number }>`
+  position: relative;
+  background-color: #262626;
+  width: ${(props) => props.width || 100}%;
+  padding: 1.5% 1%;
+  border-radius: 10px;
+`;
+
+const StyledImage = styled(Image)`
+  padding-bottom: 10%;
+`;
+
+const StyledCardTitle = styled.p`
+  font-size: 16px;
+  font-family: Arial;
+  color: #ffffff;
+  padding-bottom: 5%;
+`;
+
+export const PlaylistCard: FC<PlaylistCardProps> = (props) => {
+  const { coverImageUrl, playlistName } = props;
+
+  return (
+    <>
+      <StyledCard {...props}>
+        <StyledImage className="content" src={coverImageUrl} />
+        <StyledCardTitle>{playlistName}</StyledCardTitle>
+      </StyledCard>
+    </>
+  );
+};
---
+++ src/stories/PlaylistCard.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { PlaylistCard, PlaylistCardProps } from '../components/PlaylistCard';
+
+export default {
+  title: 'components/PlaylistCard',
+  component: PlaylistCard,
+} as Meta;
+
+const Template: Story<PlaylistCardProps> = (args: PlaylistCardProps) => (
+  <PlaylistCard {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  coverImageUrl:
+    'https://i.scdn.co/image/ab67616d0000b273233906d0f076db62a4f819d0',
+  playlistName: '洋楽',
+  description: '好きな洋楽',
+  width: 20,
+};
