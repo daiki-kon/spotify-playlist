6b52dbc65fca6411c0e1463a0e2aa70e30af3f61
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2020-12-29 23:53:52 +0900

    クリック時アニメーション追加等

--- src/components/PlaylistButton.tsx
+++ src/components/PlaylistButton.tsx
@@ -1,27 +1,49 @@
 import React, { FC, ReactNode } from 'react';
 import styled from 'styled-components';
 import 'semantic-ui-css/semantic.min.css';
+import { colorPicker, Color } from '../utils/Color';
 
 export type PlaylistButtonProps = {
   width?: number;
   children: ReactNode;
+  backGroundColor: Color;
 };
 
-const StyledButton = styled.button<{ width: number }>`
+const StyledButton = styled.button<{ backGroundColor: string; width: number }>`
   position: relative;
-  background-color: #262626;
+  background-color: ${(props) => props.backGroundColor};
   width: ${(props) => props.width}%;
-  padding: 1.5% 1%;
+  max-width: 240px;
+  padding: 10px;
   border-radius: 10px;
+  /* アウトライン削除 */
   border: none;
+  outline: none;
+  /* カーソルを変化 */
+  cursor: pointer;
+
+  /* クリック時のアニメーション */
+  :hover {
+    top: -2px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.5);
+  }
+  :active {
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.5);
+    top: 0;
+  }
 `;
 
 export const PlaylistButton: FC<PlaylistButtonProps> = (props) => {
-  const { children, width = 100 } = props;
+  const { children, backGroundColor, width = 100 } = props;
 
   return (
     <>
-      <StyledButton width={width}>{children}</StyledButton>
+      <StyledButton
+        backGroundColor={colorPicker(backGroundColor)}
+        width={width}
+      >
+        {children}
+      </StyledButton>
     </>
   );
 };
