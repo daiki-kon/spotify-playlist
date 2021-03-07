cc4801f384e4224bee41f2f54d3e639b45e42f6c
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2021-03-07 16:09:00 +0900

    editPlaylistPageにカバー画像、プレイリスト名追加

--- src/pages/EditPlaylistPage.tsx
+++ src/pages/EditPlaylistPage.tsx
@@ -6,6 +6,11 @@ import { TracksTab } from '../components/TracksTab';
 import { PlaylistTracksTableContainer } from '../containers/PlaylistTracksTableContainer';
 import { colorPicker } from '../utils/Color';
 import NoCoverImage from '../assets/NoCoverImage.png';
+import {
+  usePlaylistInfo,
+  UsePlaylistInfoResponse,
+} from '../hooks/usePlaylistInfo';
+import { getAccessToken } from '../utils/Environment';
 import 'semantic-ui-css/semantic.min.css';
 
 const StyledGrid = styled.div`
@@ -34,29 +39,38 @@ const StyledPlaylistHeader = styled.div`
 
 const StyledCoverImage = styled(Image)`
   max-height: 80px;
-  background-color: red !important;
 `;
 
 const StyledLabel = styled.label<{ textColor: string }>`
   color: ${(props) => props.textColor};
-  font-size: 30px;
+  font-size: 20px;
   font-weight: bold;
   margin-top: auto;
   margin-bottom: auto;
   margin-left: 20px;
+  margin-right: 10px;
+  /* 文字が右端まで行ったら「...」で省略する */
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 
 export const EditPlaylistPage: FC = () => {
   const { playlistId } = useParams<{ playlistId: string }>();
+
+  const playlistInfo: UsePlaylistInfoResponse = usePlaylistInfo({
+    accessToken: getAccessToken(),
+    playlistId,
+  });
 
   return (
     <StyledGrid>
       <TracksTab />
       <StylePlaylistGrid background={colorPicker('dark')}>
         <StyledPlaylistHeader>
-          <StyledCoverImage src={NoCoverImage} />
+          <StyledCoverImage src={playlistInfo.imageUrl} />
           <StyledLabel textColor={colorPicker('white')}>
-            Playlist Name
+            {playlistInfo.name}
           </StyledLabel>
         </StyledPlaylistHeader>
         <PlaylistTracksTableContainer playlistId={playlistId} />
